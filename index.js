108d35d9acef7e6e5d8e5328dfb5c5ffaf4dc49f
Junjae Lee <ejj612o@gmail.com>
2020-01-16 23:14:55 +0900

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+const RATE = {
+  morning : document.querySelector("#morning-rate"),
+  lunch   : document.querySelector("#lunch-rate"),
+  dinner  : document.querySelector("#dinner-rate")
+}
+const EATAMOUNT = document.querySelector("input #amount")
+const BLOOD = document.getElementsByName("a2"); // 여기 우짜지
+
+console.log(RATE.morning);
+console.log(RATE.lunch);
+console.log(RATE.dinner);
+
+console.log(EATAMOUNT);
+console.log(BLOOD);
